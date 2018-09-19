35adbc4c71a02374be66de04451054075425ba64
Srinivas A <ambati.sreeni@gmail.com>
2018-09-19 12:52:39 +0530

    genmake should generate makefiles files for cpp programs as well

--- build/linux.lib.mk
+++ build/linux.lib.mk
@@ -1,19 +1,21 @@
-ifdef ZFS_COMMON_OBJS
-SRCS+=$(subst .o,.c,${ZFS_COMMON_OBJS})
-endif #ZFS_COMMON_OBJS
+ifeq (${CC},g++)
+	OBJS=$(SRCS:.cpp=.o)
+else
+	OBJS=$(SRCS:.c=.o)
+endif
 
-ifdef ZFS_SHARED_OBJS
-SRCS+=$(subst .o,.c,${ZFS_SHARED_OBJS})
-endif #ZFS_SHARED_OBJS
-
-OBJS=$(SRCS:.c=.o)
 PROG=lib${LIB}.so
 CFLAGS+=${EXTRA_CFLAGS} ${LDADD}
 all:${PROG}
 ${PROG}:${OBJS} 
 	$(CC)  -shared ${LDFLAGS} ${CFLAGS} $^  -o $@ ${LIBS} 
+ifeq (${CC},g++)
+%.o:%.cpp ${SRCS}
+	$(CC) -fPIC ${CFLAGS}  -c $^
+else
 %.o:%.c ${SRCS}
 	$(CC) -fPIC ${CFLAGS}  -c $^
+endif
 
 .PHONY:clean
 clean:
--- build/linux.prog.mk
+++ build/linux.prog.mk
@@ -1,12 +1,19 @@
-OBJS=$(SRCS:.c=.o)
-
+ifeq (${CC},g++)
+	OBJS=$(SRCS:.cpp=.o)
+else
+	OBJS=$(SRCS:.c=.o)
+endif
 CFLAGS+=${EXTRA_CFLAGS} ${LDADD}
 all:${PROG}
 ${PROG}:${OBJS}
 	$(CC)  ${CFLAGS} ${OBJS} -o ${PROG} ${LIBS} ${LDFLAGS}
+ifeq (${CC},g++)
+%.o:%.cpp
+	$(CC) ${CFLAGS} -c $<
+else
 %.o:%.c
 	$(CC) ${CFLAGS} -c $< 
-
+endif
 .PHONY:clean
 clean:
 	${RM} ${OBJS} ${PROG}	
